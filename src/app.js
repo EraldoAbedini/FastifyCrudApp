f8df72981e5618ae230786f9447e60782f69d614
EraldoAbedini <abedinieraldo@gmail.com>
2022-06-15 21:25:11 +0200

    added src and test

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const fastify = require('fastify')
+
+const build = (options = {}) => {
+    const app = fastify(options)
+    return app
+}
+module.exports = { build }
--- src/server.js
+++ src/server.js
@@ -1,3 +1,15 @@
-const fastify = require('fastify')({
-    logger: true
+const { build } = require('./app')
+
+const app = build({ logger: true })
+
+
+app.get('/', (req, reply) => {
+    reply.send({ hello: "world" })
+})
+
+app.listen(3000, (error, address) => {
+    if (error) {
+        app.log.error(error)
+        process.exit(1)
+    }
 })
