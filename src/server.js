bcc8907ca4262b16615ef4f7a94d8a5dbf6bab22
EraldoAbedini <abedinieraldo@gmail.com>
2022-06-15 14:38:02 +0200

    make some changes

--- src/server.js
+++ src/server.js
@@ -1,2 +1,13 @@
-const fastify = require('fastify')
-    
+const { build } = require('./app.js')
+const app = build({ logger: true })
+
+app.get('/', (request, reply) =>
+    reply.send({ hello: 'world' })
+)
+
+app.listen(8000, (e, address) => {
+    if (e) {
+        app.log.error(e)
+        process.exit(1)
+    }
+})
